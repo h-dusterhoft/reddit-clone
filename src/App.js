82a41c9f9bda5602d402a6e875aec6a2575e62eb
H Dusterhoft <119828145+h-dusterhoft@users.noreply.github.com>
2023-08-29 12:54:40 -0500

    Import useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev');
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/webdev.json").then(res => {
+      if (res.status != 200) {
+        console.log("Error!")
+        return;
+      }
+      res.json().then(data => {
+        if (data != null) {
+          setArticles(data.data.children);
+        }
+      })
+    })
+  }, [subreddit]);
+
   return (
     <div className="App">
       <header className="App-header">
-        Header
+        <input type="text" className="input" value="webdev" />
       </header>
+      <div className="articles">
+
+      </div>
     </div>
   );
 }
