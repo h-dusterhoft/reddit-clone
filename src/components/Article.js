435f1f8fdda4bc9e75957c3103c11255bd2bd0ef
H Dusterhoft <119828145+h-dusterhoft@users.noreply.github.com>
2023-08-30 11:58:19 -0500

    Center text and edit broken img thumbnail.

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,7 +4,7 @@ function Article(props) {
     return (
         <article>
             <a href={ "https://reddit.com" + props.article.permalink } target="_blank">
-                <img src={ props.article.thumbnail } alt='' /> 
+                <img src={ props.article.thumbnail } alt='' onError={(e) => e.target.style.display='none' } /> 
                 <h3>{ props.article.title }</h3>
             </a>
         </article>
